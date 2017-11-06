55d2935e7016b60d893a43ece37f3629f42ec42a
Akshay Bhatia <akshaybhatia10@gmail.com>
2017-11-06 18:49:35 +0530

    evaluated normalized_embeddings

--- skip_gram_word2vec.py
+++ skip_gram_word2vec.py
@@ -3,6 +3,7 @@ import numpy as np
 import re
 from collections import Counter
 import random
+import time
 
 def read_data(text_file):
 	'''
@@ -69,7 +70,7 @@ def window_words(words, index, window_size=5):
 	returns a list of words in the window around the index
 	'''
 	s = np.random.randint(1, window_size+1)
-	first = index - s if (index - R) > 0 else 0
+	first = index - s if (index - s) > 0 else 0
 	last = index + s
 	close_words = set(words[first:index] + words[index+1:last+1])
 
@@ -89,7 +90,7 @@ def generate_batches(words, batch_size, window_size=5):
 			y = window_words(batch, ii, window_size)
 			targets.extend(y)
 			inputs.extend([x]*len(y))
-		yield x, y	
+		yield inputs, targets	
 
 
 # Model Hyperparameters
@@ -99,6 +100,9 @@ window_size = 5
 sampled = 100
 valid_size = 16
 valid_window = 100
+epochs = 100
+
+
 if __name__ == '__main__':
 	data = read_data('data/HarryPotter.txt')
 	#print (data[:100])
@@ -128,9 +132,56 @@ if __name__ == '__main__':
 	optimizer = tf.train.AdamOptimizer().minimize(loss)
 
 	valid_examples = np.array(random.sample(range(valid_window), valid_size//2))
-	valid_examples = np.append(valid_examples, random.sample(range(1000, 1000+valid_window), valid//2))
-
+	valid_examples = np.append(valid_examples, random.sample(range(1000, 1000+valid_window), valid_size//2))
 	valid_set = tf.constant(valid_examples, dtype=tf.int32)
 
+	norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+	normalized_embeddings = embeddings / norm
+	valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_set)
+	similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))
+
+	# Training the model
+	init = tf.global_variables_initializer()
+	session = tf.Session()
+	saver = tf.train.Saver()
 	
+	with session as sess:
+		sess.run(init)
+		i = 1
+		total_loss = 0
+		for epoch in range(epochs):
+			batches = generate_batches(final_words, batch_size, window_size)
+			start = time.time()
+			for x, y in batches:
+				l, _ = sess.run([loss, optimizer], feed_dict={inputs:x, targets:np.array(y)[:, None]})
+				total_loss += l
+
+				if i % 100 ==0:
+					end = time.time()
+					print ("Epoch: {}/{}, Iteration: {}, Average loss: {:.4f}, Time/batch: {}".format(epoch, epochs, i, total_loss, (end-start)))
+
+					total_loss = 0
+					start = time.time()
+
+				if i % 1000 == 0:
+					sim = similarity.eval()
+					for j in range(valid_size):
+						valid_word = int_to_vocab[valid_examples[j]]
+						top = 8
+						nearest = (-sim[j, :]).argsort()[1:top+1]		
+						string = 'Nearest to {} : '.format(valid_word)	
+						for k in range(top):
+							closest = int_to_vocab[nearest[k]]
+							string = string + " " + closest
+						print (string)
+				i += 1
+
+		saved_At = saver.save(sess, "checkpoints/final.ckpt")
+		final_embeddings = sess.run(normalized_embeddings)					
+
+
+
+
+
+
 
